59f90eec808a99d8cba936d23cf40cb34009e65c
Deniz Karakaş <96745481+denizkarakass@users.noreply.github.com>
2022-06-19 15:02:59 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express'); //web sunucusu 
+const app = express();
+const server = require('http').Server(app); //http server
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid'); //eşsiz oda idleri oluşturmak için
+const { ExpressPeerServer, PeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {debug:true});
+
+const room_url = uuidv4(); // oluşturduğum eşsiz oda id sini room_url diye tanımladım
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+app.use('/peerjs', peerServer);
+
+app.get('/', (req,res)=>{
+   res.redirect(`/${room_url}`);
+});
+
+app.get('/:room', (req,res)=>{
+   res.render('room',{roomId: req.params.room})
+});
+
+io.on('connection', socket =>{
+   socket.on('join-room', (roomId, userId) => {
+      socket.join(roomId);
+      socket.to(roomId)/*.broadcast*/.emit('user-connected', userId);
+      socket.on('message', message => {
+         io.to(roomId).emit('createMessage', message)
+      })
+            //console.log('kullanıcı odaya katıldı');
+   })
+})
+
+server.listen(3000); 
